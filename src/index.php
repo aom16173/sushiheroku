bca15c0ca7ecbaac4bac7594c631c3a3d3e2a054
aom16173 <aom16173@gmail.com>
2021-11-20 17:42:49 +0700

    first commit

---
+++ src/index.php
@@ -0,0 +1,97 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Sushi Website</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+    
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">SUSHI</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Meals</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Promotion</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Delivery</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Order Now</a>
+              </li>
+              
+              
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+
+<section class="header">
+    <div class="header-content">
+        <div class="container d-flex align-items-center">
+            <div class="head-content">
+            <h1>Your favourite Sushi</h1>
+            <p>awuog herognhq e[orhi nqre[o inq[ore ingvds nigio3rnges gne uifgbis oenosiu ersborf ei sfhb voiuse
+            </p>
+            <button class="btn-download">Download App</button>
+            </div>>
+        </div>
+    </div>
+</section>
+
+<section class="customizable">
+    <div class="container">
+        <h1 class="text-center">Custom your menu</h1>
+        <img src="assets/images/Sushi_top.jpg" class="img-fluid">
+    </div>
+</section>
+
+<section class="madeorder d-flex align-items-center d-flex justify-content-center">
+  <h1>The best ingredient in town Good Taste Best Price</h1>
+</section>
+
+<section class="recommended">
+  <h1 class="text-center my-5">recommended menu</h1>
+</section>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <img src="assets/images/recom1.jpg" class="img-fluid">
+      <h1>Salmon</h1>
+      <p>awuog herognhq e[orhi nqre[o inq[ore ingvds nigio3rnges gne uifgbis oenosiu ersborf ei sfhb voiuse</p>
+    </div>
+    <div class="col-md-4">
+      <img src="assets/images/recom2.jpg" class="img-fluid">
+      <h1>Tempura</h1>
+      <p>awuog herognhq e[orhi nqre[o inq[ore ingvds nigio3rnges gne uifgbis oenosiu ersborf ei sfhb voiuse</p>
+    </div>
+    <div class="col-md-4">
+      <img src="assets/images/recom3.jpg" class="img-fluid">
+      <h1>Uni</h1>
+      <p>awuog herognhq e[orhi nqre[o inq[ore ingvds nigio3rnges gne uifgbis oenosiu ersborf ei sfhb voiuse</p>
+    </div>
+  </div>
+</div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
+
